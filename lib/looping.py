d8003125d55781fe99320cb9757fda581de0b5d3
Maureen Cheptoo <cheptoomaureen31@gmail.com>
2024-05-21 12:05:07 +0300

    assignment

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,21 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 
 def happy_new_year():
-    # code goes here!
-    pass
+    for i in range(10, 0, -1):
+        print(i)
+    print("Happy New Year!")
 
-def square_integers(int_list):
-    # code goes here!
-    pass
+def square_integers(lst):
+    return [x ** 2 for x in lst]
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    for i in range(1, 101):
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
+
